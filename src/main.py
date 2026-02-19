26f2011bdb9f3e6e471e10920e58295bd03cc83b
isntrid <ridn123321@gmail.com>
2026-02-19 19:31:39 +0000

    added input() function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 
 
 def get_files():
+    
     with open("words.txt", 'r') as w:
         
         lines = w.readlines()
@@ -12,3 +13,32 @@ def get_files():
         
     return words
 
+def get_input():
+    
+    MAX_ATTEMPTS = 5
+    for _ in range(MAX_ATTEMPTS):
+        word = input("Word: ").strip().lower()
+
+        if not word:
+            print("Empty input. Try again.")
+            continue
+
+        if len(word) != 5:
+            print("Word must be 5 letters.")
+            continue
+
+        if not word.isascii():
+            print("Only standard English letters allowed.")
+            continue
+
+        if not word.isalpha():
+            print("No numbers or punctuation.")
+            continue
+        
+        return word
+    raise ValueError("Too many invalid attempts.")
+        
+def check(word):
+    
+    grey, green, yellow = [], [], []
+    
